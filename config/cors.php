43250a17633aec4aabb340514424c7b062746aad
HRJHL <125470712+HRJHL@users.noreply.github.com>
2024-08-28 14:55:55 +0900

    ㅁㅇㄹ
    ㅁㄴㅇㄹ

--- config/cors.php
+++ config/cors.php
@@ -2,9 +2,9 @@
 
 return [
 
-'paths' => ['api/*'],
+'paths' => ['*'],
 'allowed_methods' => ['*'],
-'allowed_origins' => ['http://localhost:8080'], // 요청을 허용할 출처 추가
+'allowed_origins' => ['http://3.35.133.157'], // 요청을 허용할 출처 추가
 'allowed_headers' => ['*'],
 'exposed_headers' => [],
 'max_age' => 0,
